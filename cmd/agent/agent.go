c772540438a546f4ba33aeea0e0fa01b9855bcad
Stefano Torresi <stefano@torresi.io>
2021-03-22 20:30:33 +0100

    first implementation of Consul service registration

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -1,7 +1,14 @@
 package agent
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/spf13/cobra"
+
+	"github.com/trento-project/trento/agent"
 )
 
 func NewAgentCmd() *cobra.Command {
@@ -14,15 +21,44 @@ func NewAgentCmd() *cobra.Command {
 	cmdStart := &cobra.Command{
 		Use:   "start",
 		Short: "Start the agent",
+		Run:   start,
 	}
 
 	cmdAgent := &cobra.Command{
 		Use:   "agent",
-		Short: "Intermediate agent command",
+		Short: "Command tree related to the agent component",
 	}
 
 	cmdAgent.AddCommand(cmdRegister)
 	cmdAgent.AddCommand(cmdStart)
 
 	return cmdAgent
+}
+
+func start(cmd *cobra.Command, args []string) {
+	var err error
+
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	a, err := agent.New()
+	if err != nil {
+		log.Fatal("Failed to create the agent instance: ", err)
+	}
+
+	defer func() {
+		quit := <-signals
+		log.Printf("Caught %s signal! Stopping the agent...", quit)
+
+		a.Stop()
+
+		log.Println("Agent stopped.")
+	}()
+
+	go func() {
+		err = a.Start()
+		if err != nil {
+			log.Fatal("Failed to start the agent: ", err)
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/hashicorp/consul/api v1.1.0
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0

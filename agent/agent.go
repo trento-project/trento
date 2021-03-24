9346e710a24a1e33bb4e4bf660c994d0c065490f
Stefano Torresi <stefano@torresi.io>
2021-03-24 17:51:19 +0100

    add agent configuration

--- agent/agent.go
+++ agent/agent.go
@@ -11,37 +11,62 @@ import (
 )
 
 type Agent struct {
-	consul        *consul.Client
-	consulService *consul.AgentServiceRegistration
-	ctx           context.Context
-	Stop          context.CancelFunc
+	cfg       Config
+	check     Check
+	consul    *consul.Client
+	ctx       context.Context
+	ctxCancel context.CancelFunc
 }
 
-type checkFunc func() (string, error)
+type Config struct {
+	InstanceName    string
+	DefinitionsPath string
+	TTL             time.Duration
+}
 
 func New() (*Agent, error) {
-	name, err := os.Hostname()
+	config, err := DefaultConfig()
 	if err != nil {
-		return nil, errors.Wrap(err, "could not read the hostname")
+		return nil, errors.Wrap(err, "could not create the agent configuration")
 	}
 
+	return NewWithConfig(config)
+}
+
+// returns a new instance of Agent with the given configuration
+func NewWithConfig(cfg Config) (*Agent, error) {
 	client, err := consul.NewClient(consul.DefaultConfig())
 	if err != nil {
 		return nil, errors.Wrap(err, "could not create a Consul client")
 	}
 
-	consulService := &consul.AgentServiceRegistration{
-		Name: name,
-		Tags: []string{"console-agent"},
-		Check: &consul.AgentServiceCheck{
-			TTL: "10s",
-		},
+	checker, err := NewCheck(cfg.DefinitionsPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create a Checker instance")
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, ctxCancel := context.WithCancel(context.Background())
 
-	agent := &Agent{client, consulService, ctx, cancel}
+	agent := &Agent{
+		cfg:       cfg,
+		check:     checker,
+		ctx:       ctx,
+		ctxCancel: ctxCancel,
+		consul:    client,
+	}
 	return agent, nil
+}
+
+func DefaultConfig() (Config, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return Config{}, errors.Wrap(err, "could not read the hostname")
+	}
+
+	return Config{
+		InstanceName: hostname,
+		TTL:          10 * time.Second,
+	}, nil
 }
 
 func (a *Agent) Start() error {
@@ -49,16 +74,26 @@ func (a *Agent) Start() error {
 	defer a.deregister()
 
 	log.Println("Registering the agent service with Consul...")
-	err = a.consul.Agent().ServiceRegister(a.consulService)
+
+	consulService := &consul.AgentServiceRegistration{
+		Name: a.cfg.InstanceName,
+		Tags: []string{"console-agent"},
+		Check: &consul.AgentServiceCheck{
+			Name:   "HA config checks",
+			Notes:  "Checks whether or not the HA configuration is compliant with the provided best practices",
+			TTL:    a.cfg.TTL.String(),
+			Status: consul.HealthWarning,
+		},
+	}
+
+	err = a.consul.Agent().ServiceRegister(consulService)
 	if err != nil {
 		return errors.Wrap(err, "could not register the agent service with Consul")
 	}
 	log.Println("Consul service registered.")
 
 	log.Println("Starting Consul TTL Check loop...")
-	err = a.startConsulCheckTicker(func() (string, error) {
-		return "all is good", nil
-	})
+	err = a.startConsulCheckTicker(a.check)
 	if err != nil {
 		return errors.Wrap(err, "could not start Consul TTL Check loop")
 	}
@@ -66,25 +101,25 @@ func (a *Agent) Start() error {
 
 	return nil
 }
+
+func (a *Agent) Stop() {
+	a.ctxCancel()
+}
+
 func (a *Agent) deregister() {
 	log.Println("De-registering the agent service with Consul...")
-	err := a.consul.Agent().ServiceDeregister(a.consulService.Name)
+	err := a.consul.Agent().ServiceDeregister(a.cfg.InstanceName)
 	if err != nil {
-		log.Println("An error occurred while trying to deregister the agent service with Consul: ", err)
+		log.Println("An error occurred while trying to deregister the agent service with Consul:", err)
 		return
 	}
 	log.Println("Consul service de-registered.")
 }
 
-func (a *Agent) startConsulCheckTicker(check checkFunc) error {
+func (a *Agent) startConsulCheckTicker(check Check) error {
 	a.consulCheck(check) // immediate first tick
 
-	duration, err := time.ParseDuration(a.consulService.Check.TTL)
-	if err != nil {
-		return errors.Wrap(err, "could not parse TTL duration")
-	}
-
-	ticker := time.NewTicker(duration / 2)
+	ticker := time.NewTicker(a.cfg.TTL / 2)
 	defer ticker.Stop()
 	for {
 		select {
@@ -96,24 +131,22 @@ func (a *Agent) startConsulCheckTicker(check checkFunc) error {
 	}
 }
 
-func (a *Agent) consulCheck(check checkFunc) {
+func (a *Agent) consulCheck(check Check) {
 	log.Println("Updating Consul check TTL...")
 
 	var err error
-	var ttlStatus string
 
-	checkOutput, checkErr := check()
-	if checkErr != nil {
-		checkOutput = checkErr.Error()
-		ttlStatus = consul.HealthCritical
-	} else {
-		ttlStatus = consul.HealthPassing
-	}
-
-	err = a.consul.Agent().UpdateTTL("service:"+a.consulService.Name, checkOutput, ttlStatus)
+	result, err := check()
 	if err != nil {
-		log.Println("An error occurred while trying to update TTL with Consul: ", err)
+		log.Println("An error occurred while running health checks:", err)
+		return
 	}
 
-	log.Printf("Consul check TTL updated. Status: %s.", ttlStatus)
+	err = a.consul.Agent().UpdateTTL("service:"+a.cfg.InstanceName, result.String(), result.Status)
+	if err != nil {
+		log.Println("An error occurred while trying to update TTL with Consul:", err)
+		return
+	}
+
+	log.Printf("Consul check TTL updated. Status: %s.", result.Status)
 }

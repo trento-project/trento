d6d707c4e669e65bfd02d9e3ca65020346a094a9
Xabier Arbulu Insausti <xarbulu@suse.com>
2021-07-07 09:55:35 +0200

    Fix TestSAPSystemHandler test

--- web/environments_test.go
+++ web/environments_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
@@ -14,14 +15,14 @@ import (
 	"github.com/trento-project/trento/internal/consul/mocks"
 )
 
-func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
+func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog, *mocks.KV) {
 	nodes1 := []*consulApi.Node{
 		{
 			Node:       "node1",
 			Datacenter: "dc",
 			Address:    "192.168.1.1",
 			Meta: map[string]string{
-				"trento-sap-environments": "land1",
+				"trento-sap-environments": "env1",
 			},
 		},
 		{
@@ -29,7 +30,7 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 			Datacenter: "dc",
 			Address:    "192.168.1.2",
 			Meta: map[string]string{
-				"trento-sap-environments": "land1",
+				"trento-sap-environments": "env1",
 			},
 		},
 	}
@@ -40,7 +41,7 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 			Datacenter: "dc1",
 			Address:    "192.168.1.2",
 			Meta: map[string]string{
-				"trento-sap-environments": "land2",
+				"trento-sap-environments": "env2",
 			},
 		},
 		{
@@ -48,7 +49,7 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 			Datacenter: "dc",
 			Address:    "192.168.1.3",
 			Meta: map[string]string{
-				"trento-sap-environments": "land2",
+				"trento-sap-environments": "env2",
 			},
 		},
 	}
@@ -84,16 +85,16 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 				"land1": map[string]interface{}{
 					"name": "land1",
 					"sapsystems": map[string]interface{}{
-						"sys1": map[string]interface{}{
-							"name": "sys1",
+						"PRD": map[string]interface{}{
+							"name": "PRD",
 						},
 					},
 				},
 				"land2": map[string]interface{}{
 					"name": "land2",
 					"sapsystems": map[string]interface{}{
-						"sys2": map[string]interface{}{
-							"name": "sys2",
+						"HA2": map[string]interface{}{
+							"name": "HA2",
 						},
 					},
 				},
@@ -105,8 +106,8 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 				"land3": map[string]interface{}{
 					"name": "land3",
 					"sapsystems": map[string]interface{}{
-						"sys3": map[string]interface{}{
-							"name": "sys3",
+						"HA3": map[string]interface{}{
+							"name": "HA3",
 						},
 					},
 				},
@@ -126,15 +127,15 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 	kv.On("ListMap", consul.KvEnvironmentsPath, consul.KvEnvironmentsPath).Return(filters, nil)
 
 	filterSys1 := &consulApi.QueryOptions{
-		Filter: "(Meta[\"trento-sap-environment\"] == \"env1\") and (Meta[\"trento-sap-landscape\"] == \"land1\") and (Meta[\"trento-sap-system\"] == \"sys1\")"}
+		Filter: "(Meta[\"trento-sap-environment\"] == \"env1\") and (Meta[\"trento-sap-landscape\"] == \"land1\") and (Meta[\"trento-sap-system\"] == \"PRD\")"}
 	catalog.On("Nodes", filterSys1).Return(nodes1, nil, nil)
 
 	filterSys2 := &consulApi.QueryOptions{
-		Filter: "(Meta[\"trento-sap-environment\"] == \"env1\") and (Meta[\"trento-sap-landscape\"] == \"land2\") and (Meta[\"trento-sap-system\"] == \"sys2\")"}
+		Filter: "(Meta[\"trento-sap-environment\"] == \"env1\") and (Meta[\"trento-sap-landscape\"] == \"land2\") and (Meta[\"trento-sap-system\"] == \"HA2\")"}
 	catalog.On("Nodes", filterSys2).Return(nodes1, nil, nil)
 
 	filterSys3 := &consulApi.QueryOptions{
-		Filter: "(Meta[\"trento-sap-environment\"] == \"env2\") and (Meta[\"trento-sap-landscape\"] == \"land3\") and (Meta[\"trento-sap-system\"] == \"sys3\")"}
+		Filter: "(Meta[\"trento-sap-environment\"] == \"env2\") and (Meta[\"trento-sap-landscape\"] == \"land3\") and (Meta[\"trento-sap-system\"] == \"HA3\")"}
 	catalog.On("Nodes", filterSys3).Return(nodes2, nil, nil)
 
 	health.On("Node", "node1", (*consulApi.QueryOptions)(nil)).Return(node1HealthChecks, nil, nil)
@@ -142,11 +143,11 @@ func setupEnvironmentsTest() (*mocks.Client, *mocks.Catalog) {
 	health.On("Node", "node3", (*consulApi.QueryOptions)(nil)).Return(node3HealthChecks, nil, nil)
 	health.On("Node", "node4", (*consulApi.QueryOptions)(nil)).Return(node4HealthChecks, nil, nil)
 
-	return consulInst, catalog
+	return consulInst, catalog, kv
 }
 
 func TestEnvironmentsListHandler(t *testing.T) {
-	consulInst, catalog := setupEnvironmentsTest()
+	consulInst, catalog, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -177,7 +178,7 @@ func TestEnvironmentsListHandler(t *testing.T) {
 }
 
 func TestLandscapesListHandler(t *testing.T) {
-	consulInst, catalog := setupEnvironmentsTest()
+	consulInst, catalog, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -208,7 +209,7 @@ func TestLandscapesListHandler(t *testing.T) {
 }
 
 func TestSAPSystemsListHandler(t *testing.T) {
-	consulInst, catalog := setupEnvironmentsTest()
+	consulInst, catalog, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -233,13 +234,13 @@ func TestSAPSystemsListHandler(t *testing.T) {
 	responseBody := minifyHtml(resp.Body.String())
 
 	assert.Equal(t, 200, resp.Code)
-	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/sys1\\?environment=env1&landscape=land1'\".*<td>sys1</td><td>.*passing.*</td>"), responseBody)
-	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/sys2\\?environment=env1&landscape=land2'\".*<td>sys2</td><td>.*passing.*</td>"), responseBody)
-	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/sys3\\?environment=env2&landscape=land3'\".*<td>sys3</td><td>.*critical.*</td>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/PRD\\?environment=env1&landscape=land1'\".*<td>PRD</td><td>.*passing.*</td>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/HA2\\?environment=env1&landscape=land2'\".*<td>HA2</td><td>.*passing.*</td>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile("<tr.*onclick=\"window.location='/sapsystems/HA3\\?environment=env2&landscape=land3'\".*<td>HA3</td><td>.*critical.*</td>"), responseBody)
 }
 
 func TestLandscapeHandler(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -263,7 +264,7 @@ func TestLandscapeHandler(t *testing.T) {
 }
 
 func TestLandscapeHandler404Error(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -286,7 +287,7 @@ func TestLandscapeHandler404Error(t *testing.T) {
 }
 
 func TestEnvironmentHandler(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -310,7 +311,7 @@ func TestEnvironmentHandler(t *testing.T) {
 }
 
 func TestEnvironmentHandler404Error(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -333,7 +334,50 @@ func TestEnvironmentHandler404Error(t *testing.T) {
 }
 
 func TestSAPSystemHandler(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, kv := setupEnvironmentsTest()
+
+	sapSystemMap := map[string]interface{}{
+		"PRD": map[string]interface{}{
+			"sid":  "PRD",
+			"type": 1,
+			"instances": map[string]interface{}{
+				"HDB00": map[string]interface{}{
+					"name": "HDB00",
+					"type": 1,
+					"host": "node1",
+					"sapcontrol": map[string]interface{}{
+						"instances": map[string]interface{}{
+							"host1": map[string]interface{}{
+								"Hostname":      "host1",
+								"InstanceNr":    0,
+								"HttpPort":      50013,
+								"HttpsPort":     50014,
+								"StartPriority": "0.3",
+								"Features":      "some features",
+								"Dispstatus":    "green",
+							},
+							"host2": map[string]interface{}{
+								"Hostname":      "host2",
+								"InstanceNr":    1,
+								"HttpPort":      50113,
+								"HttpsPort":     50114,
+								"StartPriority": "0.3",
+								"Features":      "some other features",
+								"Dispstatus":    "yellow",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	sapSystemPath := fmt.Sprintf(consul.KvHostsSAPSystemPath, "node1")
+	consulInst.On("WaitLock", sapSystemPath).Return(nil, nil)
+	kv.On("ListMap", sapSystemPath, sapSystemPath).Return(sapSystemMap, nil)
+
+	sapSystemPath2 := fmt.Sprintf(consul.KvHostsSAPSystemPath, "node2")
+	consulInst.On("WaitLock", sapSystemPath2).Return(nil, nil)
+	kv.On("ListMap", sapSystemPath2, sapSystemPath2).Return(sapSystemMap, nil)
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst
@@ -345,19 +389,29 @@ func TestSAPSystemHandler(t *testing.T) {
 	}
 
 	resp := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/sapsystems/sys1?environment=env1&landscape=land1", nil)
+	req, _ := http.NewRequest("GET", "/sapsystems/PRD?environment=env1&landscape=land1", nil)
 	req.Header.Set("Accept", "text/html")
 
 	app.ServeHTTP(resp, req)
 
+	responseBody := minifyHtml(resp.Body.String())
+
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.Code)
-	assert.Contains(t, resp.Body.String(), "SAP System details")
-	assert.Contains(t, resp.Body.String(), "sys1")
+	assert.Contains(t, responseBody, "SAP System details")
+	assert.Contains(t, responseBody, "PRD")
+	assert.Regexp(t, regexp.MustCompile(
+		"<tr><td>host1</td><td>0</td><td>some features</td><td>50013</td><td>50014</td><td>0.3</td><td>.*green.*</td></tr>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile(
+		"<tr><td>host2</td><td>1</td><td>some other features</td><td>50113</td><td>50114</td><td>0.3</td><td>.*yellow.*</td></tr>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile(
+		"<tr><td>host1</td><td>0</td><td>some features</td><td>50013</td><td>50014</td><td>0.3</td><td>.*green.*</td></tr>"), responseBody)
+	assert.Regexp(t, regexp.MustCompile(
+		"<tr><td>host2</td><td>1</td><td>some other features</td><td>50113</td><td>50114</td><td>0.3</td><td>.*yellow.*</td></tr>"), responseBody)
 }
 
 func TestSAPSystemHandler404Error(t *testing.T) {
-	consulInst, _ := setupEnvironmentsTest()
+	consulInst, _, _ := setupEnvironmentsTest()
 
 	deps := DefaultDependencies()
 	deps.consul = consulInst

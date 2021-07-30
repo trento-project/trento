32005e4754afb73bbfd1437936c567a08fa763c9
Xabier Arbulu Insausti <xarbulu@suse.com>
2021-07-30 12:29:36 +0200

    Implement a collapsable accordion for the check catalog

--- web/services/mocks/ChecksService.go
+++ web/services/mocks/ChecksService.go
@@ -34,3 +34,26 @@ func (_m *ChecksService) GetChecksCatalog() (map[string]*models.Check, error) {
 
 	return r0, r1
 }
+
+// GetChecksCatalogByGroup provides a mock function with given fields:
+func (_m *ChecksService) GetChecksCatalogByGroup() (map[string]map[string]*models.Check, error) {
+	ret := _m.Called()
+
+	var r0 map[string]map[string]*models.Check
+	if rf, ok := ret.Get(0).(func() map[string]map[string]*models.Check); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[string]map[string]*models.Check)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}

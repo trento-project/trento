aab6a1a1fe9825377295a1c3adf498a05de5414d
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-08-12 17:39:20 +0200

    Add tags javascript and style
    Co-authored-by: Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>

--- web/frontend/javascripts/tables.js
+++ web/frontend/javascripts/tables.js
@@ -1,7 +1,7 @@
 // ajax operation to reload the table on pagination operations
 // query can be `page` or `per_page`
 function reloadTable(path) {
-  $.get(path, function(response){
+  $.get(path, function (response) {
     var table = $(response).find('.table-responsive');
     $('.table-responsive').replaceWith(table);
     var nav = $(response).find('.pagination-wrap');
@@ -10,12 +10,13 @@ function reloadTable(path) {
     if (health != undefined) {
       $('.health-container').replaceWith(health);
     }
+    $(window).trigger('table:reloaded');
   });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   // pagination events
-  $('body').on('click', '.page-item', function() {
+  $('body').on('click', '.page-item', function () {
     var href = new URL(window.location.href);
     href.searchParams.set('page', this.value);
     path = href.pathname + href.search;
@@ -23,7 +24,7 @@ $(document).ready(function() {
     history.pushState(undefined, '', href);
   });
 
-  $('body').on('click', '.pagination-wrap .dropdown-item', function() {
+  $('body').on('click', '.pagination-wrap .dropdown-item', function () {
     var href = new URL(window.location.href);
     href.searchParams.set('per_page', this.textContent);
     path = href.pathname + href.search;
@@ -31,7 +32,7 @@ $(document).ready(function() {
     history.pushState(undefined, '', href);
   });
 
-  $('body').on('change', '.selectpicker', function() {
+  $('body').on('change', '.selectpicker', function () {
     var href = new URL(window.location.href);
     href.searchParams.delete(this.name)
     values = $(this).val();

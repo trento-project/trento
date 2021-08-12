e7fe9659010fe56996e86f4c9dc7774bf5b19b22
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-08-12 17:41:02 +0200

    Add sap systems tags column and filter
    Co-authored-by: Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>

--- web/sap_systems.go
+++ web/sap_systems.go
@@ -3,16 +3,19 @@ package web
 import (
 	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 	"github.com/trento-project/trento/internal"
 	"github.com/trento-project/trento/internal/consul"
 	"github.com/trento-project/trento/internal/hosts"
 	"github.com/trento-project/trento/internal/sapsystem"
+	"github.com/trento-project/trento/internal/tags"
 )
 
 type SAPSystemRow struct {
 	SID            string
+	Tags           []string
 	InstancesTable []*InstanceRow
 }
 
@@ -27,7 +30,7 @@ type InstanceRow struct {
 
 type SAPSystemsTable []*SAPSystemRow
 
-func NewSAPSystemsTable(sapSystemsList sapsystem.SAPSystemsList, hostList hosts.HostList) SAPSystemsTable {
+func NewSAPSystemsTable(sapSystemsList sapsystem.SAPSystemsList, hostList hosts.HostList, client consul.Client) (SAPSystemsTable, error) {
 	var sapSystemsTable SAPSystemsTable
 	rowsBySID := make(map[string]*SAPSystemRow)
 
@@ -38,8 +41,15 @@ func NewSAPSystemsTable(sapSystemsList sapsystem.SAPSystemsList, hostList hosts.
 
 		sapSystem, ok := rowsBySID[s.SID]
 		if !ok {
+			t := tags.NewTags(client, "sapsystems", s.SID)
+			sapsystemTags, err := t.GetAll()
+			if err != nil {
+				return nil, err
+			}
+
 			sapSystem = &SAPSystemRow{
-				SID: s.SID,
+				SID:  s.SID,
+				Tags: sapsystemTags,
 			}
 			rowsBySID[s.SID] = sapSystem
 		}
@@ -83,19 +93,36 @@ func NewSAPSystemsTable(sapSystemsList sapsystem.SAPSystemsList, hostList hosts.
 		sapSystemsTable = append(sapSystemsTable, row)
 	}
 
-	return sapSystemsTable
+	sort.Slice(sapSystemsTable, func(i, j int) bool {
+		return sapSystemsTable[i].SID < sapSystemsTable[j].SID
+	})
+
+	return sapSystemsTable, nil
 }
 
-func (t SAPSystemsTable) filter(sid []string) SAPSystemsTable {
+func (t SAPSystemsTable) filter(sid []string, tags []string) SAPSystemsTable {
 	var filteredSAPSystemsTable SAPSystemsTable
-	if len(sid) == 0 {
-		return t
-	}
 
 	for _, r := range t {
-		if internal.Contains(sid, r.SID) {
-			filteredSAPSystemsTable = append(filteredSAPSystemsTable, r)
+		if len(sid) > 0 && !internal.Contains(sid, r.SID) {
+			continue
 		}
+
+		if len(tags) > 0 {
+			tagFound := false
+			for _, t := range tags {
+				if internal.Contains(r.Tags, t) {
+					tagFound = true
+					break
+				}
+			}
+
+			if !tagFound {
+				continue
+			}
+		}
+
+		filteredSAPSystemsTable = append(filteredSAPSystemsTable, r)
 	}
 
 	return filteredSAPSystemsTable
@@ -116,11 +143,29 @@ func (t SAPSystemsTable) GetAllSIDs() []string {
 	return sids
 }
 
+func (t SAPSystemsTable) GetAllTags() []string {
+	var tags []string
+	set := make(map[string]struct{})
+
+	for _, r := range t {
+		for _, tag := range r.Tags {
+			_, ok := set[tag]
+			if !ok {
+				set[tag] = struct{}{}
+				tags = append(tags, tag)
+			}
+		}
+	}
+
+	return tags
+}
+
 func NewSAPSystemListHandler(client consul.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var sapSystemsList sapsystem.SAPSystemsList
 		query := c.Request.URL.Query()
 		sidFilter := query["sid"]
+		tagsFilter := query["tags"]
 
 		hostList, err := hosts.Load(client, "", nil)
 		if err != nil {
@@ -140,8 +185,13 @@ func NewSAPSystemListHandler(client consul.Client) gin.HandlerFunc {
 			}
 		}
 
-		sapSystemsTable := NewSAPSystemsTable(sapSystemsList, hostList)
-		sapSystemsTable = sapSystemsTable.filter(sidFilter)
+		sapSystemsTable, err := NewSAPSystemsTable(sapSystemsList, hostList, client)
+		if err != nil {
+			_ = c.Error(err)
+			return
+		}
+
+		sapSystemsTable = sapSystemsTable.filter(sidFilter, tagsFilter)
 
 		c.HTML(http.StatusOK, "sapsystems.html.tmpl", gin.H{
 			"SAPSystemsTable": sapSystemsTable,

1450595e893723cbf536ca981613dde71e8eee0a
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-09-01 12:24:00 +0200

    Update tests

--- web/clusters_test.go
+++ web/clusters_test.go
@@ -401,7 +401,7 @@ func TestClusterHandlerHANA(t *testing.T) {
 	assert.Regexp(t, regexp.MustCompile("<strong>CIB last written:</strong><br><span.*>Wed Jun 30 18:11:37 2021</span>"), minified)
 	assert.Regexp(t, regexp.MustCompile("<strong>HANA secondary sync state:</strong><br><span.*>SFAIL</span>"), minified)
 	// Nodes
-	assert.Regexp(t, regexp.MustCompile("<td><a.*href=/hosts/test_node_1.*>test_node_1</a></td><td>192.168.1.1</td><td>10.123.123.123</td>"), minified)
+	assert.Regexp(t, regexp.MustCompile("<td><a.*href=/hosts/test_node_1.*>test_node_1</a></td><td>192.168.1.1</td><td>10.123.123.123</td><td><span .*>HANA Primary</span>"), minified)
 	assert.Regexp(t, regexp.MustCompile("<td><a.*href=/hosts/test_node_2.*>test_node_2</a></td><td>192.168.1.2</td>"), minified)
 	// Resources
 	assert.Regexp(t, regexp.MustCompile("<td>sbd</td><td>stonith:external/sbd</td><td>Started</td><td>active</td><td>0</td>"), minified)

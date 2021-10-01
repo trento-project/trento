6150de9b040fdc7de60da1e4e6fc4d4ed90f6679
Alessio Biancalana <alessio.biancalana@suse.com>
2021-10-01 16:42:48 +0200

    State 'skipped' result explicitly

--- web/frontend/components/ChecksTable/CheckResultIcon.js
+++ web/frontend/components/ChecksTable/CheckResultIcon.js
@@ -3,6 +3,7 @@ import React from 'react';
 const PASSING = 'passing';
 const WARNING = 'warning';
 const CRITICAL = 'critical';
+const SKIPPED = 'skipped';
 
 const CheckResultIcon = ({ result }) => {
   switch (result) {
@@ -12,6 +13,8 @@ const CheckResultIcon = ({ result }) => {
       return <i className="eos-icons eos-18 text-warning">warning</i>;
     case CRITICAL:
       return <i className="eos-icons eos-18 text-danger">error</i>;
+    case SKIPPED:
+      return <i className="eos-icons eos-18 text-muted">fiber_manual_record</i>;
     default:
       return <i className="eos-icons eos-18 text-muted">fiber_manual_record</i>;
   }

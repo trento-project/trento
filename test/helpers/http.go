40e0c408c63d4993dbc76cc0c5b7bed8aee8f132
nelsonkopliku <nelson.kopliku@suse.com>
2021-10-21 14:39:42 +0200

    Documented how to mock http requests

--- test/helpers/http.go
+++ test/helpers/http.go
@@ -2,10 +2,23 @@ package helpers
 
 import "net/http"
 
-// RoundTripFunc 
+// Usage
+
+// &http.Client{Transport: helpers.RoundTripFunc(func(req *http.Request) *http.Response {
+// 	 bodyBytes, _ := ioutil.ReadAll(req.Body)
+//
+// 	 suite.EqualValues(expectedBody, string(bodyBytes))
+//
+// 	 suite.Equal(req.URL.String(), expectedUrl)
+// 	 return &http.Response{
+// 		StatusCode: returnedStatusCode,
+// 	 }
+// }}
+
+// RoundTripFunc Needed to Mock Http client requests and responses
 type RoundTripFunc func(req *http.Request) *http.Response
 
-// RoundTrip .
+// RoundTrip implements the RoundTripper interface
 func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 	return f(req), nil
 }

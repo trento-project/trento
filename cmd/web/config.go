3697aa819771fff35f52f3abe74529e6ff0c2621
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-10-25 09:42:15 +0200

    Refactor configuration loading in a new file

---
+++ cmd/web/config.go
@@ -0,0 +1,43 @@
+package web
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"github.com/trento-project/trento/web"
+)
+
+func LoadConfig() (*web.Config, error) {
+	enablemTLS := viper.GetBool("enable-mtls")
+	cert := viper.GetString("cert")
+	key := viper.GetString("key")
+	ca := viper.GetString("ca")
+
+	if enablemTLS {
+		var err error
+
+		if cert == "" {
+			err = fmt.Errorf("you must provide a server ssl certificate")
+		}
+		if key == "" {
+			err = errors.Wrap(err, "you must provide a key to enable mTLS")
+		}
+		if ca == "" {
+			err = errors.Wrap(err, "you must provide a CA ssl certificate")
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &web.Config{
+		Host:          viper.GetString("host"),
+		Port:          viper.GetInt("port"),
+		CollectorPort: viper.GetInt("collector-port"),
+		EnablemTLS:    enablemTLS,
+		Cert:          cert,
+		Key:           key,
+		CA:            ca,
+	}, nil
+}

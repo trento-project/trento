d1b1d3b29e62aba29ec16a487ec4779cfde237ef
Stefano Torresi <stefano.torresi@suse.com>
2021-10-29 14:35:59 +0200

    remove the host swagger declaration

--- docs/api/docs.go
+++ docs/api/docs.go
@@ -708,7 +708,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "localhost:8080",
+	Host:        "",
 	BasePath:    "/api",
 	Schemes:     []string{"http"},
 	Title:       "Trento API",
--- web/api.go
+++ web/api.go
@@ -17,7 +17,6 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /api
 // @schemes http
 

1ed5822903c5250cfa44a33450d5e6de86bd70ee
nelsonkopliku <nelson.kopliku@suse.com>
2021-11-02 17:36:27 +0100

    Wired the Telemetry readmodel to the DB and added the new TelemetryProjector to the Registry

--- web/datapipeline/projector_registry.go
+++ web/datapipeline/projector_registry.go
@@ -6,8 +6,8 @@ type ProjectorRegistry []Projector
 
 // InitInitProjectorsRegistry initialize the ProjectorRegistry
 func InitProjectorsRegistry(db *gorm.DB) ProjectorRegistry {
-
 	return ProjectorRegistry{
 		NewClustersProjector(db),
+		NewHostTelemetryProjector(db),
 	}
 }

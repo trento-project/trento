70056ca43430a5fe8f8c368e4ebab1b610067c01
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-11-08 09:49:55 +0100

    Add new hosts service

---
+++ web/services/hosts_next.go
@@ -0,0 +1,89 @@
+package services
+
+import (
+	"github.com/lib/pq"
+	"github.com/trento-project/trento/web/entities"
+	"github.com/trento-project/trento/web/models"
+	"gorm.io/gorm"
+)
+
+//go:generate mockery --name=HostsNextService --inpackage --filename=hosts_next_mock.go
+type HostsNextService interface {
+	GetAll(filters map[string][]string) (models.HostList, error)
+	GetAllSIDs() ([]string, error)
+	GetAllTags() ([]string, error)
+}
+
+type hostsNextService struct {
+	db *gorm.DB
+}
+
+func NewHostsNextService(db *gorm.DB) *hostsNextService {
+	return &hostsNextService{db}
+}
+
+func (s *hostsNextService) GetAll(filters map[string][]string) (models.HostList, error) {
+	var hosts []entities.Host
+	db := s.db.Preload("Tags")
+
+	if tags, ok := filters["tags"]; ok {
+		db = db.Where("agent_id IN (?)", s.db.Model(&models.Tag{}).
+			Select("resource_id").
+			Where("resource_type = ?", models.TagHostResourceType).
+			Where("value IN ?", tags),
+		)
+	}
+
+	if sids, ok := filters["sids"]; ok {
+		if len(sids) > 0 {
+			db = db.Where("sids && ?", pq.Array(sids))
+		}
+	}
+
+	err := db.Find(&hosts).Error
+	if err != nil {
+		return nil, err
+	}
+
+	var hostList models.HostList
+	for _, host := range hosts {
+		hostList = append(hostList, host.ToModel())
+	}
+
+	return hostList, nil
+}
+
+func (s *hostsNextService) GetAllSIDs() ([]string, error) {
+	var sids pq.StringArray
+
+	err := s.db.Model(&entities.Host{}).
+		Where("sids IS NOT NULL").
+		Distinct().
+		Pluck("unnest(sids)", &sids).
+		Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return []string(sids), nil
+}
+
+func (s *hostsNextService) GetAllTags() ([]string, error) {
+	var tags []string
+
+	err := s.db.
+		Model(&models.Tag{
+			ResourceType: models.TagHostResourceType,
+		}).
+		Distinct().
+		Pluck("value", &tags).
+		Error
+
+	if err != nil {
+		return nil, err
+
+	}
+
+	return tags, nil
+}

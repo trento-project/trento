b2341da21bd56b3e892f8747cbf2477d679aca6a
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-11-12 18:14:28 +0100

    Update clusters service test

--- web/services/clusters_mock.go
+++ web/services/clusters_mock.go
@@ -34,3 +34,72 @@ func (_m *MockClustersService) GetAll(filters map[string][]string) (models.Clust
 
 	return r0, r1
 }
+
+// GetAllClusterTypes provides a mock function with given fields:
+func (_m *MockClustersService) GetAllClusterTypes() ([]string, error) {
+	ret := _m.Called()
+
+	var r0 []string
+	if rf, ok := ret.Get(0).(func() []string); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]string)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetAllSIDs provides a mock function with given fields:
+func (_m *MockClustersService) GetAllSIDs() ([]string, error) {
+	ret := _m.Called()
+
+	var r0 []string
+	if rf, ok := ret.Get(0).(func() []string); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]string)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetAllTags provides a mock function with given fields:
+func (_m *MockClustersService) GetAllTags() ([]string, error) {
+	ret := _m.Called()
+
+	var r0 []string
+	if rf, ok := ret.Get(0).(func() []string); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]string)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}

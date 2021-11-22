6c3f5cee4cabf227404d341889c0db940b83a22b
nelsonkopliku <nelson.kopliku@suse.com>
2021-11-22 18:05:43 +0100

    Adds tests for the telemetry engine

--- test/helpers/http.go
+++ test/helpers/http.go
@@ -2,23 +2,39 @@ package helpers
 
 import "net/http"
 
-// Usage
-
-// &http.Client{Transport: helpers.RoundTripFunc(func(req *http.Request) *http.Response {
-// 	 bodyBytes, _ := ioutil.ReadAll(req.Body)
-//
-// 	 suite.EqualValues(expectedBody, string(bodyBytes))
-//
-// 	 suite.Equal(req.URL.String(), expectedUrl)
-// 	 return &http.Response{
-// 		StatusCode: returnedStatusCode,
-// 	 }
-// }}
-
 // RoundTripFunc Needed to Mock Http client requests and responses
+//
+// Usage
+// &http.Client{
+// 	 Transport: helpers.RoundTripFunc(func(req *http.Request) *http.Response {
+// 	    bodyBytes, _ := ioutil.ReadAll(req.Body)
+//
+// 	 	suite.EqualValues(expectedBody, string(bodyBytes))
+//
+// 	 	suite.Equal(req.URL.String(), expectedUrl)
+// 	 	return &http.Response{
+// 			StatusCode: returnedStatusCode,
+// 	 	}
+//     }),
+// }
 type RoundTripFunc func(req *http.Request) *http.Response
 
-// RoundTrip implements the RoundTripper interface
+// ErroringRoundTripFunc Needed to Mock Http client that returns an error even before the request is made
+//
+// Usage
+// &http.Client{
+// 	 Transport: helpers.ErroringRoundTripFunc(func() error {
+// 	 	return fmt.Errorf("some error")
+// 	 }),
+// }
+type ErroringRoundTripFunc func() error
+
+// RoundTripFunc implements the RoundTripper interface
 func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 	return f(req), nil
+}
+
+// ErroringRoundTripFunc implements the RoundTripper interface
+func (f ErroringRoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return nil, f()
 }

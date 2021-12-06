cbf7f3479304237f8e2531040de086ec6b0efdaf
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-12-06 17:22:13 +0100

    Update sap systems related entities/models

---
+++ web/entities/sap_system.go
@@ -0,0 +1,78 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+	"github.com/trento-project/trento/web/models"
+)
+
+type SAPSystemInstance struct {
+	ID                      string `gorm:"primaryKey"`
+	AgentID                 string `gorm:"primaryKey"`
+	Type                    string
+	SID                     string `gorm:"column:sid"`
+	InstanceNumber          string `gorm:"primaryKey"`
+	Features                string
+	Description             string
+	SystemReplication       string
+	SystemReplicationStatus string
+	DBHost                  string
+	DBName                  string
+	Tenants                 pq.StringArray `gorm:"type:text[]"`
+	Host                    *Host          `gorm:"foreignKey:AgentID"`
+	UpdatedAt               time.Time
+	Tags                    []*models.Tag `gorm:"foreignKey:ResourceID"`
+}
+
+type SAPSystemInstances []*SAPSystemInstance
+
+func (s SAPSystemInstances) ToModel() []*models.SAPSystem {
+	set := make(map[string]*models.SAPSystem)
+
+	for _, i := range s {
+
+		sapSystem, ok := set[i.ID]
+		if !ok {
+			// TODO: move to Tags entity when we will have it
+			var tags []string
+			for _, tag := range i.Tags {
+				tags = append(tags, tag.Value)
+			}
+
+			sapSystem = &models.SAPSystem{
+				ID:     i.ID,
+				Type:   i.Type,
+				SID:    i.SID,
+				DBName: i.DBName,
+				DBHost: i.DBHost,
+				Tags:   tags,
+			}
+			set[i.ID] = sapSystem
+		}
+
+		sapSystemInstance := &models.SAPSystemInstance{
+			InstanceNumber:          i.InstanceNumber,
+			Features:                i.Features,
+			SystemReplication:       i.SystemReplication,
+			SystemReplicationStatus: i.SystemReplicationStatus,
+			Type:                    i.Type,
+			SID:                     i.SID,
+		}
+
+		if i.Host != nil {
+			sapSystemInstance.ClusterName = i.Host.ClusterName
+			sapSystemInstance.ClusterID = i.Host.ClusterID
+			sapSystemInstance.HostID = i.Host.AgentID
+			sapSystemInstance.Hostname = i.Host.Name
+		}
+
+		sapSystem.Instances = append(sapSystem.Instances, sapSystemInstance)
+	}
+
+	var sapSystems []*models.SAPSystem
+	for _, sapSystem := range set {
+		sapSystems = append(sapSystems, sapSystem)
+	}
+	return sapSystems
+}
--- web/models/sap_system.go
+++ web/models/sap_system.go
@@ -1,13 +1,44 @@
 package models
 
+const (
+	SAPSystemTypeApplication = "application"
+	SAPSystemTypeDatabase    = "database"
+)
+
 type SAPSystem struct {
-	ID        string
-	SID       string
-	Type      string
-	Instances []*SAPSystemInstance
+	ID               string
+	SID              string
+	Type             string
+	Instances        []*SAPSystemInstance
+	AttachedDatabase *SAPSystem
+	DBName           string
+	DBHost           string
+	Tags             []string
+	// TODO: this is frontend specific, should be removed
+	HasDuplicatedSID bool
 }
 
 type SAPSystemInstance struct {
-	InstanceNumber string
-	Features       string
+	Type                    string
+	SID                     string
+	Features                string
+	InstanceNumber          string
+	SystemReplication       string
+	SystemReplicationStatus string
+	ClusterName             string
+	ClusterID               string
+	HostID                  string
+	Hostname                string
+}
+
+type SAPSystemList []*SAPSystem
+
+func (s SAPSystem) GetAllInstances() []*SAPSystemInstance {
+	instances := s.Instances
+
+	if s.AttachedDatabase != nil {
+		instances = append(instances, s.AttachedDatabase.Instances...)
+	}
+
+	return instances
 }

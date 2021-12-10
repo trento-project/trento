6148b05fa329ca13bd4c9a7f3497c950fa0a38ca
Alessio Biancalana <alessio.biancalana@suse.com>
2021-12-10 11:02:07 +0100

    Fix host UI tests

--- web/services/hosts_mock.go
+++ web/services/hosts_mock.go
@@ -127,6 +127,29 @@ func (_m *MockHostsService) GetByID(_a0 string) (*models.Host, error) {
 	return r0, r1
 }
 
+// GetByName provides a mock function with given fields: _a0
+func (_m *MockHostsService) GetByName(_a0 string) (*models.Host, error) {
+	ret := _m.Called(_a0)
+
+	var r0 *models.Host
+	if rf, ok := ret.Get(0).(func(string) *models.Host); ok {
+		r0 = rf(_a0)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.Host)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetCount provides a mock function with given fields:
 func (_m *MockHostsService) GetCount() (int, error) {
 	ret := _m.Called()

739d0b45878703c2cc5dc7a4f3fac66e270e291b
nelsonkopliku <nelson.kopliku@suse.com>
2021-12-16 10:02:04 +0100

    Refactored EULA middleware with PremiumDetection support

--- web/middlewares.go
+++ web/middlewares.go
@@ -8,30 +8,23 @@ import (
 	"github.com/trento-project/trento/web/services"
 )
 
-func EulaMiddleware(settings services.SettingsService, subscriptions services.SubscriptionsService) gin.HandlerFunc {
+func EulaMiddleware(premiumDetection services.PremiumDetectionService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		isPremium, err := subscriptions.IsTrentoPremium()
+		requiresEulaAcceptance, err := premiumDetection.RequiresEulaAcceptance()
+
 		if err != nil {
 			log.Error(err)
-			c.HTML(http.StatusInternalServerError, "error.html.tmpl", gin.H{"Error": "Error checking wether the instance is premium or not"})
-			c.Abort()
-			return
-		}
-		eulaAccepted, err := settings.IsEulaAccepted()
-		if err != nil {
-			log.Error(err)
-			c.HTML(http.StatusInternalServerError, "error.html.tmpl", gin.H{"Error": "EULA Error :-("})
+			c.HTML(http.StatusInternalServerError, "error.html.tmpl", gin.H{"Error": "Error checking whether the instance is premium or not"})
 			c.Abort()
 			return
 		}
 
-		if c.Request.URL.Path != "/accept-eula" && c.Request.URL.Path != "/eula" && !eulaAccepted && isPremium {
+		if c.Request.URL.Path != "/accept-eula" && c.Request.URL.Path != "/eula" && requiresEulaAcceptance {
 			c.Redirect(http.StatusFound, "/eula")
 			c.HTML(http.StatusOK, "eula.html.tmpl", gin.H{"Title": "License agreement"})
 			c.Abort()
 			return
 		}
 		c.Next()
-		return
 	}
 }
--- web/middlewares_test.go
+++ web/middlewares_test.go
@@ -5,18 +5,15 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/trento-project/trento/web/services"
 )
 
 func TestEulaMiddleware(t *testing.T) {
-	mockedSettingsService := new(services.MockSettingsService)
-	mockedSettingsService.On("AcceptEula").Return(nil)
-	mockedSettingsService.On("IsEulaAccepted").Return(false, nil)
-	mockedSettingsService.On("InitializeIdentifier").Return(uuid.MustParse("59fd8017-b7fd-477b-9ebe-b658c558f3e9"), nil)
+	mockedPremiumDetection := new(services.MockPremiumDetection)
+	mockedPremiumDetection.On("RequiresEulaAcceptance").Return(true, nil)
 	deps := setupTestDependencies()
-	deps.settingsService = mockedSettingsService
+	deps.premiumDetection = mockedPremiumDetection
 	config := setupTestConfig()
 	app, err := NewAppWithDeps(config, deps)
 	if err != nil {
@@ -32,42 +29,11 @@ func TestEulaMiddleware(t *testing.T) {
 	assert.Contains(t, resp.Body.String(), "License agreement")
 }
 
-func TestEulaMiddlewareNotPremium(t *testing.T) {
-	mockedSettingsService := new(services.MockSettingsService)
-	mockedSettingsService.On("AcceptEula").Return(nil)
-	mockedSettingsService.On("IsEulaAccepted").Return(false, nil)
-	mockedSettingsService.On("InitializeIdentifier").Return(uuid.MustParse("59fd8017-b7fd-477b-9ebe-b658c558f3e9"), nil)
-
-	mockedSubscriptionsService := new(services.MockSubscriptionsService)
-	mockedSubscriptionsService.On("IsTrentoPremium").Return(false, nil)
-
-	deps := setupTestDependencies()
-
-	deps.settingsService = mockedSettingsService
-	deps.subscriptionsService = mockedSubscriptionsService
-
-	config := setupTestConfig()
-	app, err := NewAppWithDeps(config, deps)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	resp := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/", nil)
-
-	app.webEngine.ServeHTTP(resp, req)
-
-	assert.Equal(t, 200, resp.Code)
-	assert.Contains(t, resp.Body.String(), "Blue Horizon for SAP")
-}
-
 func TestEulaMiddlewareLettingThrough(t *testing.T) {
-	mockedSettingsService := new(services.MockSettingsService)
-	mockedSettingsService.On("AcceptEula").Return(nil)
-	mockedSettingsService.On("IsEulaAccepted").Return(true, nil)
-	mockedSettingsService.On("InitializeIdentifier").Return(uuid.MustParse("59fd8017-b7fd-477b-9ebe-b658c558f3e9"), nil)
+	mockedPremiumDetection := new(services.MockPremiumDetection)
+	mockedPremiumDetection.On("RequiresEulaAcceptance").Return(false, nil)
 	deps := setupTestDependencies()
-	deps.settingsService = mockedSettingsService
+	deps.premiumDetection = mockedPremiumDetection
 	config := setupTestConfig()
 	app, err := NewAppWithDeps(config, deps)
 	if err != nil {
@@ -84,12 +50,10 @@ func TestEulaMiddlewareLettingThrough(t *testing.T) {
 }
 
 func TestEulaMiddlewareError(t *testing.T) {
-	mockedSettingsService := new(services.MockSettingsService)
-	mockedSettingsService.On("AcceptEula").Return(nil)
-	mockedSettingsService.On("IsEulaAccepted").Return(false, errors.New("EULA error"))
-	mockedSettingsService.On("InitializeIdentifier").Return(uuid.MustParse("59fd8017-b7fd-477b-9ebe-b658c558f3e9"), nil)
+	mockedPremiumDetection := new(services.MockPremiumDetection)
+	mockedPremiumDetection.On("RequiresEulaAcceptance").Return(false, errors.New("EULA error"))
 	deps := setupTestDependencies()
-	deps.settingsService = mockedSettingsService
+	deps.premiumDetection = mockedPremiumDetection
 	config := setupTestConfig()
 	app, err := NewAppWithDeps(config, deps)
 	if err != nil {

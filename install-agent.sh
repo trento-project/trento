460efc686b49200731121fee403784427a490906
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-01-03 14:41:31 +0100

    Add mTLS to install-agent script

--- install-agent.sh
+++ install-agent.sh
@@ -15,7 +15,11 @@ Usage:
 Arguments:
   --ssh-address     The address to which the trento-agent should be reachable for ssh connection by the runner for check execution.
   --server-ip       The trento server ip.
-  --rolling         Use the factory/rolling-release version instead of the stable one.
+  --enable-mtls     Enable mTLS secure communication between the agent and the server.
+  --cert            The path to the TLS certificate file. Required if --enable-mtls is set.
+  --key             The path to the TLS key file. Required if --enable-mtls is set.
+  --ca              The path to the TLS CA file. Required if --enable-mtls is set.
+  --rolling         Use the rolling version instead of the stable one.
   --use-tgz         Use the trento tar.gz file from GH releases rather than the RPM
   --help            Print this help.
 END
@@ -36,6 +40,10 @@ fi
 ARGUMENT_LIST=(
     "ssh-address:"
     "server-ip:"
+    "enable-mtls"
+    "cert:"
+    "key:"
+    "ca:"
     "rolling"
     "use-tgz"
 )
@@ -63,14 +71,34 @@ while [[ $# -gt 0 ]]; do
             SERVER_IP=$2
             shift 2
         ;;
-        
+
+        --enable-mtls)
+            ENABLE_MTLS=true
+            shift 1
+        ;;
+
+        --cert)
+            CERT=$2
+            shift 2
+        ;;
+
+        --key)
+            KEY=$2
+            shift 2
+        ;;
+
+        --ca)
+            CA=$2
+            shift 2
+        ;;
+
         --rolling)
-            USE_ROLLING=1
+            USE_ROLLING=true
             shift 1
         ;;
         
         --use-tgz)
-            USE_TGZ=1
+            USE_TGZ=true
             shift 1
         ;;
         
@@ -85,12 +113,16 @@ AGENT_CONFIG_FILE="$AGENT_CONFIG_PATH/agent.yaml"
 AGENT_CONFIG_TEMPLATE='
 ssh-address: @SSH_ADDRESS@
 collector-host: @COLLECTOR_HOST@
+enable-mtls: @ENABLE_MTLS@
+cert: @CERT@
+key: @KEY@
+ca: @CA@
 '
 
 . /etc/os-release
 if [[ ! $PRETTY_NAME =~ "SUSE" ]]; then
     echo "Warning: non-SUSE operating system, forcing --use-tgz"
-    USE_TGZ=1
+    USE_TGZ=true
 fi
 
 echo "Installing trento-agent..."
@@ -113,6 +145,49 @@ function configure_installation() {
     if [[ -z "$SERVER_IP" ]]; then
         read -rp "Please provide the server IP: " SERVER_IP </dev/tty
     fi
+
+    configure_mtls
+}
+
+function configure_mtls() {
+    if [[ -n "$ENABLE_MTLS" ]]; then
+        if [[ -z "$CERT" ]]; then
+            read -rp "Please provide the TLS certificate path: " CERT </dev/tty
+
+        fi
+        CERT=$(normalize_path "$CERT") || {
+            echo "Path to the TLS cert file does not exist, please try again."
+            exit 1
+        }
+
+        if [[ -z "$KEY" ]]; then
+            read -rp "Please provide the TLS key path: " KEY </dev/tty
+        fi
+        KEY=$(normalize_path "$KEY") || {
+            echo "Path to the TLS key file does not exist, please try again."
+            exit 1
+        }
+
+        if [[ -z "$CA" ]]; then
+            read -rp "Please provide the TLS CA path: " CA </dev/tty
+        fi
+        CA=$(normalize_path "$CA") || {
+            echo "Path to the TLS CA file does not exist, please try again."
+            exit 1
+        }
+    fi
+}
+
+function normalize_path() {
+    local path=$1
+    local absolute_path
+
+    path="${path/#\~/$HOME}"
+    absolute_path=$(realpath -q -e "$path") || {
+        exit 1
+    }
+
+    echo "$absolute_path"
 }
 
 function install_trento() {
@@ -122,6 +197,7 @@ function install_trento() {
     fi
 
     if [[ -n "$USE_TGZ" ]] ; then
+        echo "* Downloading trento tar.gz from GitHub..."
         install_trento_tgz
     else
         install_trento_rpm
@@ -179,13 +255,19 @@ function install_trento_tgz() {
 }
 
 function setup_trento() {
+    local enable_mtls=${ENABLE_MTLS:-"false"}
+
     echo "* Generating trento-agent config..."
 
     mkdir -p ${AGENT_CONFIG_PATH} && touch ${AGENT_CONFIG_FILE}
 
     echo "$AGENT_CONFIG_TEMPLATE" |
         sed "s|@COLLECTOR_HOST@|${SERVER_IP}|g" |
-        sed "s|@SSH_ADDRESS@|${SSH_ADDRESS}|g" \
+        sed "s|@SSH_ADDRESS@|${SSH_ADDRESS}|g" |
+        sed "s|@ENABLE_MTLS@|${enable_mtls}|g" |
+        sed "s|@CERT@|${CERT}|g" |
+        sed "s|@KEY@|${KEY}|g" |
+        sed "s|@CA@|${CA}|g" \
             > ${AGENT_CONFIG_FILE}
 }
 

21a8be1bf92dba3eacdbf3fe535ca29d04b4dc4d
arbulu89 <xarbulu@suse.com>
2022-02-09 12:04:17 +0100

    Add tests to the new prometheus and exporters code

--- web/services/hosts_mock.go
+++ web/services/hosts_mock.go
@@ -148,6 +148,29 @@ func (_m *MockHostsService) GetCount() (int, error) {
 	return r0, r1
 }
 
+// GetExportersState provides a mock function with given fields: hostname
+func (_m *MockHostsService) GetExportersState(hostname string) (map[string]bool, error) {
+	ret := _m.Called(hostname)
+
+	var r0 map[string]bool
+	if rf, ok := ret.Get(0).(func(string) map[string]bool); ok {
+		r0 = rf(hostname)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[string]bool)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(hostname)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Heartbeat provides a mock function with given fields: agentID
 func (_m *MockHostsService) Heartbeat(agentID string) error {
 	ret := _m.Called(agentID)
